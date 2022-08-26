79d8e2653657b38bd0e5c985f7339e021912acb6
DaeJin Jeong <devwithu@naver.com>
2022-08-26 09:07:44 +0900

    first commit

---
+++ YoutubeLiveChat/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace YoutubeLiveChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(655, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 407);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "https://www.youtube.com/watch?v=1-iS7LArMPA";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(765, 345);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ YoutubeLiveChat/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace YoutubeLiveChat
+{
+    public partial class Form1 : Form
+    {
+        String webPath;
+        String workingPath;
+        CefSettings settings;
+        ChromiumWebBrowser browser;
+
+        void initChrome()
+        {
+            settings = new CefSettings();
+            settings.LogFile = workingPath + "debug.log";
+            settings.CefCommandLineArgs.Add("persistant_sessioin_cookie", "1");
+            settings.CachePath = workingPath;
+            Cef.Initialize(settings, performDependencyCheck: true);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            workingPath = Environment.ExpandEnvironmentVariables("%userprofile%") + "\\documents\\s2cache\\";
+            initChrome();
+            Console.WriteLine("1");
+        }
+
+        public void processNewLine(String name, String text)
+        {
+            Console.WriteLine(name + " >>> " + text);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webPath = null;
+            panel1.Dock = DockStyle.Fill;
+            browser = new ChromiumWebBrowser(textBox1.Text);
+            browser.BrowserSettings = new BrowserSettings();
+            browser.JavascriptObjectRepository.Register("bound", new cefCustomObject(this), false, options: BindingOptions.DefaultBinder);
+            browser.FrameLoadEnd += (object o, FrameLoadEndEventArgs fleea) =>
+            {
+                Console.WriteLine("aa");
+
+                if (webPath == null && fleea.Frame.Url.Split('=')[0] == "https://www.youtube.com/live_chat?continuation")
+                {
+                    Console.WriteLine("a");
+                    browser.Load(webPath = fleea.Frame.Url);
+                }
+                else if(webPath == fleea.Frame.Url)
+                {
+                    Console.WriteLine("b");
+                    browser.ShowDevTools();
+                    fleea.Frame.ExecuteJavaScriptAsync("(async function(){ await CefSharp.BindObjectAsync('boundAsync', 'bound');   var last = ''; setInterval(function(){ (function(l){ if(last!=(cid = l[l.length-1].id))for(var i = l.length; i--;) { if(last==l[i].id)return (last=cid); if(l[i].children[1].children[2].textContent)bound.onText(l[i].children[1].children[1].children[0].textContent, l[i].children[1].children[2].textContent); }; last = cid; })(document.getElementsByTagName('yt-live-chat-text-message-renderer')); }, 1000);     })()");
+                }
+            };
+            browser.Dock = DockStyle.Fill;
+            panel1.Controls.Add(browser);
+
+        }
+
+        class cefCustomObject
+        {
+            private static Form1 mainForm;
+            public cefCustomObject(Form1 form)
+            {
+                mainForm = form;
+            }
+
+            public void onText(String name, String text)
+            {
+                mainForm.processNewLine(name, text);
+            }
+        }
+    }
+}
